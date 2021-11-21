26bd62842aec0bbb76a1ca0c839b07cda6ed2a59
NKAndrei <andrei.iuliannechita@gmail.com>
2021-11-21 13:53:25 +0000

    Add project files.

---
+++ Data/Nechita_Andrei_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nechita_Andrei_Lab8.Models;
+
+namespace Nechita_Andrei_Lab8.Data
+{
+    public class Nechita_Andrei_Lab8Context : DbContext
+    {
+        public Nechita_Andrei_Lab8Context (DbContextOptions<Nechita_Andrei_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nechita_Andrei_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Nechita_Andrei_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Nechita_Andrei_Lab8.Models.Category> Category { get; set; }
+    }
+}
